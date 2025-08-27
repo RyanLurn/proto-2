b585803c150713400656919a910e11a61087ce47
Ryan Luong <lumintriet@gmail.com>
2025-08-27 21:47:54 +0700

    Add shadcn/ui and react scan.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,12 @@
-export default function HomePage() {
-  return <h1 className="text-3xl">Home Page</h1>;
+"use client";
+
+import { toast } from "sonner";
+import { Button } from "@/components/ui/button";
+
+export default function Home() {
+  return (
+    <div className="flex h-screen w-screen items-center justify-center">
+      <Button onClick={() => toast.info("Hello")}>Click me</Button>
+    </div>
+  );
 }
